fe57eb06b81289156b050f8b79490adaffb9ba23
FFF <litew9@gmail.com>
2017-02-05 08:38:37 +0300

    Russian translation

---
+++ translations/systemdatascope-rus.ts
@@ -0,0 +1,327 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AppStatus</name>
+    <message>
+        <source>&lt;b&gt;Folder with the collectd databases while running:&lt;/b&gt; </source>
+        <translation>Директория для хранения данных процесса collectd (когда приложение запущено)</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Folder with the collectd databases while the daemon is stopped:&lt;/b&gt; </source>
+        <translation>Директория для хранения данных процесса collectd (когда приложение остановлено)</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationWindowPL</name>
+    <message>
+        <source>Zoom In</source>
+        <translation>Увеличить масштаб</translation>
+    </message>
+    <message>
+        <source>Zoom Out</source>
+        <translation>Уменьшить масштаб</translation>
+    </message>
+    <message>
+        <source>Now</source>
+        <translation>Сейчас</translation>
+    </message>
+    <message>
+        <source>&lt;---</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>---&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>1 hour</source>
+        <translation>1 час</translation>
+    </message>
+    <message>
+        <source>4 hours</source>
+        <translation>4 часа</translation>
+    </message>
+    <message>
+        <source>8 hours</source>
+        <translation>8 часов</translation>
+    </message>
+    <message>
+        <source>12 hours</source>
+        <translation>12 часов</translation>
+    </message>
+    <message>
+        <source>24 hours</source>
+        <translation>24 часа</translation>
+    </message>
+    <message>
+        <source>3 days</source>
+        <translation>3 дня</translation>
+    </message>
+    <message>
+        <source>1 week</source>
+        <translation>1 неделя</translation>
+    </message>
+    <message>
+        <source>4 weeks</source>
+        <translation>4 недели</translation>
+    </message>
+    <message>
+        <source>100 days</source>
+        <translation>100 дней</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <source>Report</source>
+        <translation>Отчёт</translation>
+    </message>
+</context>
+<context>
+    <name>ColumnLayoutDialog</name>
+    <message>
+        <source>Show Details...</source>
+        <translation>Показать детали...</translation>
+    </message>
+</context>
+<context>
+    <name>GraphList</name>
+    <message>
+        <source>Welcome</source>
+        <translation>Добро пожаловать</translation>
+    </message>
+    <message>
+        <source>Empty page with undefined title</source>
+        <translation>Пустая страница без названия</translation>
+    </message>
+</context>
+<context>
+    <name>MessageAboutPL</name>
+    <message>
+        <source>version: </source>
+        <translation>версия: </translation>
+    </message>
+</context>
+<context>
+    <name>MessageErrorPL</name>
+    <message>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPL</name>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Run collectd</source>
+        <translation>Запускать collectd</translation>
+    </message>
+    <message>
+        <source>Enable collectd on boot</source>
+        <translation>Запускать collectd при старте системы</translation>
+    </message>
+    <message>
+        <source>collectd folder while running, full pathname [/tmp/collectd/&lt;hostname&gt;]: </source>
+        <translation>директория запущеннего collectd, полный путь [/tmp/collectd/&lt;hostname&gt;]: </translation>
+    </message>
+    <message>
+        <source>collectd folder when stopped, full pathname [~/.local/share/collectd/&lt;hostname&gt;]: </source>
+        <translation>директория остановленнго collectd, полный путь [~/.local/share/collectd/&lt;hostname&gt;]: </translation>
+    </message>
+    <message>
+        <source>Update graphs interval [seconds]:</source>
+        <translation>Интервал обновления графиков (сек.):</translation>
+    </message>
+    <message>
+        <source>Graph height in pixels:</source>
+        <translation>Высота графика, пикселей:</translation>
+    </message>
+    <message>
+        <source>Graphs: Title font size:</source>
+        <translation>Графики: размер текста заголовка:</translation>
+    </message>
+    <message>
+        <source>Graphs: Axis font size:</source>
+        <translation>Графики:размер отметок на осях  </translation>
+    </message>
+    <message>
+        <source>Graphs: Unit font size:</source>
+        <translation> </translation>
+    </message>
+    <message>
+        <source>Graphs: Legend font size:</source>
+        <translation>Графики: размер текста легенды</translation>
+    </message>
+    <message>
+        <source>Load new graph definitions from URL:</source>
+        <translation>Загрузить определения новых графиков по URL:</translation>
+    </message>
+    <message>
+        <source>Generate graph definitions using systemdatascope-makeconfig:</source>
+        <translation>Сгенерировать определения графиков используя systemdatascope-makeconfig</translation>
+    </message>
+    <message>
+        <source>collectd folder while running</source>
+        <translation>директория запущеннего collectd</translation>
+    </message>
+    <message>
+        <source>Folder with the collectd databases while running. For example, /tmp/collectd/&lt;hostname&gt;</source>
+        <translation>Директория для хранения данных процесса collectd (когда приложение запущено). Например, /tmp/collectd/&lt;hostname&gt;</translation>
+    </message>
+    <message>
+        <source>collectd folder while stopped</source>
+        <translation>директория остановленного collectd</translation>
+    </message>
+    <message>
+        <source>Folder with the collectd databases while the daemon is stopped. For example, ~/.local/share/collectd/&lt;hostname&gt;</source>
+        <translation>Директория для хранения данных процесса collectd (когда приложение остановлено). Например, ~/.local/share/collectd/&lt;hostname&gt;</translation>
+    </message>
+    <message>
+        <source>Update graphs interval [seconds]</source>
+        <translation>Интервал обновления графиков (сек.)</translation>
+    </message>
+    <message>
+        <source>Graphs: Title font size</source>
+        <translation>размер названия графика</translation>
+    </message>
+    <message>
+        <source>Graphs: Axis font size</source>
+        <translation>размер отметки на оси</translation>
+    </message>
+    <message>
+        <source>Graphs: Unit font size</source>
+        <translation>размер юнита</translation>
+    </message>
+    <message>
+        <source>Graphs: Legend font size</source>
+        <translation>Графики: размер текста легенды</translation>
+    </message>
+    <message>
+        <source>Graph definitions can be generated by running systemdatascope-makeconfig automatically</source>
+        <translation>Графики определений могут быть сгенерированы используя автоматический запуск systemdatascope-makeconfig</translation>
+    </message>
+    <message>
+        <source>Generate definitions</source>
+        <translation>Сгенерировать определения</translation>
+    </message>
+    <message>
+        <source>The graph definitions can be loaded from remote or local URL (use file:// for local URL). For that, type URL to graph definition in JSON format and press Load definitions button below. Note that acceping the new settings will not load the definitions</source>
+        <translation>Определения графиков могут быть загружены локально или через URL (используйте file:// для локального URL). Введите URL на определение графика (определение в формате  JSON) и нажмите кнопку &quot;Загрузить определения&quot;. Учтите, что сохранение настроек на текущей странице не производит автоматическую загрузку определений графиков</translation>
+    </message>
+    <message>
+        <source>Interface configuration URL</source>
+        <translation>URL конфигурации интерфейса</translation>
+    </message>
+    <message>
+        <source>Load definitions</source>
+        <translation>Загрузить определения</translation>
+    </message>
+    <message>
+        <source>Report: height in pixels:</source>
+        <translation>высота, пикселей</translation>
+    </message>
+    <message>
+        <source>Graphs: height in pixels</source>
+        <translation>высота, пикселей</translation>
+    </message>
+    <message>
+        <source>Report: height in pixels</source>
+        <translation>высота, пикселей</translation>
+    </message>
+</context>
+<context>
+    <name>StatusPL</name>
+    <message>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+</context>
+<context>
+    <name>TimeControlPL</name>
+    <message>
+        <source>Time axis control</source>
+        <translation>Ось времени</translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation>Масштабирование</translation>
+    </message>
+    <message>
+        <source>Shift</source>
+        <translation>Смещение</translation>
+    </message>
+    <message>
+        <source>Time window duration</source>
+        <translation>Интервал времени</translation>
+    </message>
+    <message>
+        <source>1 hour</source>
+        <translation>1 час</translation>
+    </message>
+    <message>
+        <source>4 hours</source>
+        <translation>4 часа</translation>
+    </message>
+    <message>
+        <source>8 hours</source>
+        <translation>8 часов</translation>
+    </message>
+    <message>
+        <source>12 hours</source>
+        <translation>12 часов</translation>
+    </message>
+    <message>
+        <source>24 hours</source>
+        <translation>24 часа</translation>
+    </message>
+    <message>
+        <source>3 days</source>
+        <translation>3 дня</translation>
+    </message>
+    <message>
+        <source>1 week</source>
+        <translation>1 неделя</translation>
+    </message>
+    <message>
+        <source>4 weeks</source>
+        <translation>4 недели</translation>
+    </message>
+    <message>
+        <source>100 days</source>
+        <translation>100 дней</translation>
+    </message>
+    <message>
+        <source>1 year</source>
+        <translation>1 год</translation>
+    </message>
+    <message>
+        <source>Main graphs</source>
+        <translation>Главные графики</translation>
+    </message>
+    <message>
+        <source>2 hours</source>
+        <translation>2 часа</translation>
+    </message>
+    <message>
+        <source>Cover graphs</source>
+        <translation>Графики обложки</translation>
+    </message>
+</context>
+</TS>
